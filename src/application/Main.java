b567cc79d681b0f05ea294be61f5559b6c180f2a
JoseV-001 <Leaphaz@gmail.com>
2025-11-25 22:13:40 -0300

    feat: Versão 01 - Implementação via classe concreta

---
+++ src/application/Main.java
@@ -0,0 +1,24 @@
+package application;
+
+import model.entities.Product;
+import util.PriceUpdate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.00));
+        list.add(new Product("HD Case", 80.90));
+
+        list.forEach(new PriceUpdate());
+
+        list.forEach(System.out::println);
+
+    }
+}
